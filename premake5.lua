8a42f48e9fc0a41e456f63c1e80201998ed9b120
Lasse Siniluoto <c6sila00@students.oamk.fi>
2020-07-18 18:05:18 +0300

    Added premake build system

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Crystal"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Crystal"
+	location "Crystal"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir    ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CRYSTAL_PLATFORM_WINDOWS",
+			"CRYSTAL_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CRYSTAL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CRYSTAL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CRYSTAL_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir    ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Crystal/src"
+	}
+
+	links
+	{
+		"Crystal"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CRYSTAL_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CRYSTAL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CYSTAL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CYSTAL_DIST"
+		optimize "On"
