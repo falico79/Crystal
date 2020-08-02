e392b39bd2e2ed6738ab5ef95db33ae3d4dcc89e
Lasse Siniluoto <c6sila00@students.oamk.fi>
2020-08-02 11:14:50 +0300

    Added Basic Shader class

--- Crystal/src/Crystal/Application.h
+++ Crystal/src/Crystal/Application.h
@@ -9,6 +9,8 @@
 
 #include "Crystal/ImGui/ImGuiLayer.h"
 
+#include "Crystal/Renderer/Shader.h"
+
 namespace Crystal {
 
 	class CRYSTAL_API Application
@@ -35,6 +37,7 @@ namespace Crystal {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
