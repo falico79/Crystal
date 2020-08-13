e36bfa4da5ebe2eb5b72f5847721bdfaa6e3ffda
Lasse Siniluoto <c6sila00@students.oamk.fi>
2020-08-13 07:32:15 +0300

    Window resize

--- Crystal/src/Crystal/Application.cpp
+++ Crystal/src/Crystal/Application.cpp
@@ -50,6 +50,7 @@ namespace Crystal {
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(OnWindowResize));
 
 		CRYSTAL_CORE_TRACE("{0}", e);
 
@@ -63,15 +64,17 @@ namespace Crystal {
 
 	void Application::Run()
 	{
-		while (m_running)
+		while (m_Running)
 		{
 
 			float time = glfwGetTime();
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
-
-			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate(timestep);
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+			}
 
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
@@ -84,8 +87,23 @@ namespace Crystal {
 
 	bool Application::OnWindowClose(WindowCloseEvent& e)
 	{
-		m_running = false;
+		m_Running = false;
 		return true;
+	}
+
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+
+		m_Minimized = false;
+		Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+
+		return false;
 	}
 
 }
