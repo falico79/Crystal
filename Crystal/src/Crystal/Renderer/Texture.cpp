322a9fd4d0aa0df17e06a9fadfa7ba8a8fd8ba3e
Lasse Siniluoto <c6sila00@students.oamk.fi>
2020-08-16 08:41:44 +0300

    Single Shader 2D Renderer

--- Crystal/src/Crystal/Renderer/Texture.cpp
+++ Crystal/src/Crystal/Renderer/Texture.cpp
@@ -5,12 +5,25 @@
 #include "Platform/OpenGL/OpenGLTexture.h"
 
 namespace Crystal {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None: CRYSTAL_CORE_ASSERT(false, "RenderAPI::None is currently not supported"); return nullptr;
+			case RendererAPI::API::OpenGL: return CreateRef<OpenGLTexture2D>(width, height);
+
+		}
+
+		CRYSTAL_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::API::None: CRYSTAL_CORE_ASSERT(false, "RenderAPI::None is currently not supported"); return nullptr;
-		case RendererAPI::API::OpenGL: return std::make_shared<OpenGLTexture2D>(path);
+			case RendererAPI::API::None: CRYSTAL_CORE_ASSERT(false, "RenderAPI::None is currently not supported"); return nullptr;
+			case RendererAPI::API::OpenGL: return CreateRef<OpenGLTexture2D>(path);
 
 		}
 
--- Crystal/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Crystal/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -9,6 +9,20 @@ namespace Crystal {
 
 
 
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+		: m_Width(width), m_Height(height)
+	{
+		m_InternalFormat = GL_RGBA8, m_DataFormat = GL_RGBA;
+		glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+		glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	}
+
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
 		: m_Path(path)
 	{
@@ -21,6 +35,8 @@ namespace Crystal {
 		m_Height = height;
 
 		GLenum internalFormat = 0, dataFormat = 0;
+		m_InternalFormat = internalFormat;
+		m_DataFormat = dataFormat;
 		if (channels == 4)
 		{
 			internalFormat = GL_RGBA8;
@@ -31,6 +47,9 @@ namespace Crystal {
 			internalFormat = GL_RGB8;
 			dataFormat = GL_RGB;
 		}
+
+		m_InternalFormat = internalFormat;
+		m_DataFormat = dataFormat;
 
 		CRYSTAL_CORE_ASSERT(internalFormat & dataFormat, "Format not supported!");
 
@@ -51,6 +70,13 @@ namespace Crystal {
 	OpenGLTexture2D::~OpenGLTexture2D()
 	{
 		glDeleteTextures(1, &m_RendererID);
+	}
+
+	void OpenGLTexture2D::SetData(void* data, uint32_t size)
+	{
+		uint32_t bpp = m_DataFormat == GL_RGBA ? 4 : 3;
+		CRYSTAL_CORE_ASSERT(size == m_Width * m_Height * bpp, "Data must be entire texture!");
+		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
 	}
 
 	void OpenGLTexture2D::Bind(uint32_t slot) const
